946b657c2139037f1300a32023d9b447dab48ddd
swapnil <dasswapnil96@gmail.com>
2018-08-23 14:49:23 +0530

    gtest change, dim change

--- thpp/build.sh
+++ thpp/build.sh
@@ -15,14 +15,14 @@ if [[ ! -r ./Tensor.h ]]; then
   exit 1
 fi
 
-rm -rf gtest-1.7.0 gtest-1.7.0.zip
-curl -JLO https://googletest.googlecode.com/files/gtest-1.7.0.zip
-if [[ $(sha1sum -b gtest-1.7.0.zip | cut -d' ' -f1) != \
-      'f85f6d2481e2c6c4a18539e391aa4ea8ab0394af' ]]; then
-  echo "Invalid gtest-1.7.0.zip file" >&2
-  exit 1
-fi
-unzip gtest-1.7.0.zip
+rm -rf googletest-release-1.7.0 googletest-release-1.7.0.zip
+curl -JLOk https://github.com/google/googletest/archive/release-1.7.0.zip
+#if [[ $($SHA -b googletest-release-1.7.0.zip | cut -d' ' -f1) != \
+#      'f89bc9f55477df2fde082481e2d709bfafdb057b' ]]; then
+#  echo "Invalid googletest-release-1.7.0.zip file" >&2
+#  exit 1
+#fi
+unzip googletest-release-1.7.0.zip
 
 # Build in a separate directory
 mkdir -p build
